c9652891b3cb8083bf5042f66d601dba51353ee3
Vukoman Pejic <vukpe@LAPTOP-60LSV4G0.(none)>
2024-10-03 19:33:42 +0200

    add routes for comments also into middleware to protect them

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
     Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
     Route::delete('/posts/{id}', [PostController::class, 'delete'])->name('posts.delete');
+    Route::post('/posts/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('/comments/{id}', [CommentController::class, 'delete'])->name('comments.delete');
 });
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-
-Route::post('/posts/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::delete('/comments/{id}', [CommentController::class, 'delete'])->name('comments.delete');
 
 require __DIR__.'/auth.php';
