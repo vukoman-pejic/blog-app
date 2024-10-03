5b909c4870e89f26b84366b111d848b9a1480719
Vukoman Pejic <vukpe@LAPTOP-60LSV4G0.(none)>
2024-10-03 17:37:22 +0200

    specify the fillable fields in 'Post' and 'Comment' model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,8 @@ class Comment extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['post_id', 'user_id', 'comment'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,8 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id', 'title', 'content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
